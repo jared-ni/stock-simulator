1343d519acf84c71f892adb3b575001f29757264
milkteadj <50958750+milkteadj@users.noreply.github.com>
2021-11-02 15:27:41 -0400

    Update helpers.py

--- stock-trading-simulator-m/helpers.py
+++ stock-trading-simulator-m/helpers.py
@@ -32,8 +32,10 @@ def login_required(f):
     """
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if session.get("user_id") is None:
+        # session.get("user_id")
+        if session["user_id"] is None:
             return redirect("/login")
+        
         return f(*args, **kwargs)
     return decorated_function
 
