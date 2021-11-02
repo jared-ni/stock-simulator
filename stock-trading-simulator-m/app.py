d8918055253a074b651106534a7dd2b6bc67950c
milkteadj <50958750+milkteadj@users.noreply.github.com>
2021-11-02 15:23:30 -0400

    Update app.py

--- stock-trading-simulator-m/app.py
+++ stock-trading-simulator-m/app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
